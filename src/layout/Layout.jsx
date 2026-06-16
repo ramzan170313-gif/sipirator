5dc83e78c607e1ab10fde2db867e138944968f36
renejaytzy <ijman709@gmail.com>
2026-06-16 15:11:39 +0600

    restructure folders

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import Header from "../components/Header/Header";
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
  function Layout({ children}){
     return(
         <>
